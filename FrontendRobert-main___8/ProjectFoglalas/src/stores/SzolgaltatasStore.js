fe201fdd730803b8fecf14cf5279176b4096e7fc
Tóth Anna <153619435+Anna2000T@users.noreply.github.com>
2025-03-14 10:41:22 +0100

    Add files via upload

---
+++ FrontendRobert-main___8/ProjectFoglalas/src/stores/SzolgaltatasStore.js
@@ -0,0 +1,100 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+import axios from 'axios'
+import { Toast } from 'bootstrap'
+
+
+
+export const useSzolgaltatasok = defineStore('Szolgáltatások', () => {
+  const szolgaltatasokList = ref([])
+  const dateAdat = ref("")
+
+
+
+
+  const szolgaltatasokGet = async () =>{   
+
+    try{
+    const response = await axios.get('http://localhost:3000/szolgaltatasok');
+    szolgaltatasokList.value = response.data
+    
+    }
+
+    catch(err)
+    {
+      console.log(err)
+    }
+
+  }
+
+  const IdopontPut = async (id,idopont,foglaloNev,foglaloTelefonszama) =>{   
+
+    try{
+    const response = await axios.patch(`http://localhost:3000/szolgaltatasok/${id}`,{
+      idopont : idopont,
+      foglaloNev : foglaloNev,
+      foglaloTelefonszama
+
+    }
+  )
+
+    console.log(response.data)
+
+    if(response.status === 200)
+    {
+      alert('Successfully booked!!')
+    }
+    
+    }
+
+    catch(err)
+    {
+      console.log(err)
+    }
+
+  }
+
+  const  IdopontDelete = async (id) =>{   
+
+    try{
+    const response = await axios.patch(`http://localhost:3000/szolgaltatasok/${id}`,{
+      idopont : "",
+      foglaloNev: "",
+      foglaloTelefonszama: ""
+    }
+
+  )
+  if(response.status === 200)
+  {
+    alert('Succesfully deleted!!')
+  }
+
+
+    console.log(response.data)
+
+    
+    }
+
+    catch(err)
+    {
+      console.log(err)
+    }
+
+  }
+
+
+
+
+  const adatbevetel =  (adat) =>{
+
+    dateAdat.value = adat
+
+
+  }
+
+
+
+
+  return { szolgaltatasokGet, szolgaltatasokList,IdopontPut,IdopontDelete, adatbevetel,dateAdat}
+}
+)
---
+++ FrontendRobert-main___8/ProjectFoglalas/src/views/Foglalas.vue
@@ -0,0 +1,101 @@
+
+<template>
+
+ 
+  <form v-on:submit.prevent="save">
+
+    <h2  class="m-4">Welcome to our datepicket website</h2>
+
+<h3 class="m-3">Please pick a date: </h3>
+
+<VueDatePicker  v-model="date" inline showWeek class="w-full sm:w-[30rem] naptar" :min-date="new Date()"  :year-range="[2025, 2026]" :disabled-week-days="[6, 0]" :min-time="{ hours: 9, minutes: 30 }" :max-time="{ hours: 19, minutes: 30 }"></VueDatePicker>
+
+<button type="submit">Send</button>
+
+
+  
+
+</form>
+
+</template>
+
+<script setup>
+
+
+import { ref } from 'vue';
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
+import router from '@/router';
+import { useSzolgaltatasok } from '@/stores/SzolgaltatasStore.js';
+
+
+
+
+const adat = useSzolgaltatasok()
+const date = ref(new Date(2025, 1));
+
+
+const save = () =>{
+
+
+
+  if(date.value === null)
+{
+  alert('Please select a date!')
+}
+
+else
+{
+  
+  console.log(date.value)
+  adat.adatbevetel(date.value)
+  adat.dateAdat
+  console.log(adat.dateAdat)
+  router.push("szolgaltatas")
+ 
+}
+
+
+
+
+
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+</script>
+
+<style scoped>
+
+button{
+  border-radius: 20px;
+  border-color: rgb(231, 222, 222);
+  display: block;
+  margin: auto ;
+  width: 10vh;
+  
+
+}
+
+h2 , h3{
+  text-align: center;
+}
+.naptar{
+  margin: auto;
+  display: block;
+  width: 500px;
+  
+
+}
+
+
+</style>
